ff6f1037deb9458b17333abce68ab3a2596b9d47
Archimedes Firmino <archimedessfirmino@gmail.com>
2023-06-12 21:41:53 -0300

    Tornando a API REST através de Annotations.

---
+++ src/main/java/com/archimedes/demoapieclipse/TrecoRepository.java
@@ -0,0 +1,7 @@
+package com.archimedes.demoapieclipse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrecoRepository extends JpaRepository<Treco, Long> {
+	
+}
