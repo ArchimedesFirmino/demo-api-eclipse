20e78fa962be39af3a88b7a2f498e7d1ea2cef99
Archimedes Firmino <archimedessfirmino@gmail.com>
2023-06-15 21:40:02 -0300

    Adição do Put e do Patch aos métodos.

--- src/main/java/com/archimedes/demoapieclipse/treco/model/Treco.java
+++ src/main/java/com/archimedes/demoapieclipse/treco/model/Treco.java
@@ -1,30 +1,72 @@
 package com.archimedes.demoapieclipse.treco.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class Treco {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(nullable = false)
 	private Long id;
-	
+
+	@Column(length = 63, nullable = false)
+	private String date = "" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
 	@Column(length = 63, nullable = false)
 	private String name;
-	
+
 	@Column(length = 127)
 	private String description;
+
+	@Column(length = 31)
+	private String status = "on";
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 }
--- src/main/java/com/archimedes/demoapieclipse/users/model/Users.java
+++ src/main/java/com/archimedes/demoapieclipse/users/model/Users.java
@@ -5,16 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+
 public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,4 +22,38 @@ public class Users {
 
 	@Column(length = 63, nullable = false)
 	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
